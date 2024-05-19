256fdf5c945127e12601b4368c24771c057c58b3
Toukir\Toukir Rahman <to.toukir.rahman@gmail.com>
2024-05-20 01:50:47 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
+
 // Selecting window and header elements
 var wind = window;
 var sticky = document.querySelector('.header-area');
 
+
 // Adding scroll event listener to window
 wind.addEventListener('scroll', function() {
-    // Getting current scroll position
     var scroll = wind.scrollY || wind.pageYOffset;
-    // Checking scroll position and adding/removing class accordingly
     if (scroll < 150) {
         sticky.classList.remove('menu-fixed');
     } else {
         sticky.classList.add('menu-fixed');
     }
 });
+
 
 // Preloader
 window.addEventListener('load', function() {
